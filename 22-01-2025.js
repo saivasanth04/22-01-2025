ec266a80b01b09a5ecc3f68e0ae02a55cdbb6463
saivasanth04 <perurisaivasanth04@gmail.com>
2025-01-23 11:28:08 +0530

    nice ra

--- 22-01-2025.js
+++ 22-01-2025.js
@@ -4,3 +4,41 @@ let p2=new Promise((resolve,reject)=>{
     resolve("Scusses")
 });
 console.log(p2);
+let p3=new Promise((resolve,reject)=>{
+    reject("failure")
+});
+console.log(p3);
+p2.then((res)=>{
+    console.log(res);
+})
+p3.catch((rej)=>{
+    console.log(rej);
+}).finally(
+    (res)=>{console.log("sai vasanth")}
+)
+console.log(p3)
+function fetche(){
+    let x = fetch("https://jsonplaceholder.typicode.com/users");
+    x.then(async res=>{
+        const data = await res.json();
+        console.log(data);
+    })
+}
+fetche();
+let p=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("resolved")
+    },4000)
+})
+async function demo(){
+    console.log("start");
+    let x=await p;
+    console.log(x);
+    console.log("end")
+}
+async function fetchUsers(){
+let response=await fetch("https://jsonplaceholder.typicode.com/users")
+let pamper=await response.json()
+console.log(pamper)
+}
+fetchUsers()
